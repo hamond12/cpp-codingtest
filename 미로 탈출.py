dc7d8279c94477219bc6864d588c0baf5af3eb95
Juyoung Song <80014509+sjy1410@users.noreply.github.com>
2022-09-21 12:08:19 +0900

    이해안가는거? 외우면 그만이야~

--- 미로 탈출.py
+++ 미로 탈출.py
@@ -1,42 +1,38 @@
-from collections import deque
+import collections
 
-# 가로, 세로을 공백을 기준으로 구분하여 입력 받기
-n, m = map(int, input().split())
+n, m = map(int, input().split())  # 세로, 가로
 
-# 2차원 리스트의 맵 정보 입력 받기
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input().split())))
+maze = [list(map(int, input().split())) for i in range(n)]
 
-# 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
+#이동할 네 방향 정의(상하좌우)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-
+#bfs->시작지점에서 가까운 노드부터 그래프의 모든 노드 차례대로 탐색 
 def bfs(x, y):
-    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
-    queue = deque()
-    queue.append((x, y))
-    # 큐가 빌 때까지 반복하기
-    while queue:
+
+    #큐 구현을 위해 deque라이브러리 호출
+    queue = collections.deque([(x, y)])
+
+    #시작지점의 값은 1
+    maze[x][y] = 1 
+
+    #큐가 빌때까지 반복
+    while queue: 
+
         x, y = queue.popleft()
-        # 현재 위치에서 4가지 방향으로의 위치 확인
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            # 미로 찾기 공간을 벗어난 경우 무시
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            # 벽인 경우 무시
-            if graph[nx][ny] == 0:
-                continue
-            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
+
+        #현재 위치에서 4방향으로 위치 확인
+        for i in range(4): 
+            nx, ny = x+dx[i], y+dy[i]
+
+            #미로찾기 공간을 벗어나지 않거나 탐색한 요소의 값이 1일때 
+            if 0 <= nx < n and 0 <= ny < m and maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y] + 1 
                 queue.append((nx, ny))
-    # 가장 오른쪽 아래까지의 최단 거리 반환
-    return graph[n - 1][m - 1]
+                
+    #미로의 도착점에 해당하는 값 출력
+    return maze[n-1][m-1]
 
-
-# BFS를 수행한 결과 출력
+#배열은 0부터 시작-> 미로 시작 위치(1,1) = maze[0][0] 
 print(bfs(0, 0))
